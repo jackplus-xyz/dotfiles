fc33033699dd0ba2f753e8fbabf95b692eb57919
Jack Huang <jackplus.xyz@icloud.com>
2024-03-25 15:30:43 -0700

    style(colorscheme): change default colorscheme to onedark
    Replace github-nvim-theme with onedark due to breaking change in
    treesitter and causing no syntax highlighting for markdown files

--- nvim/lua/plugins/auto-dark-mode.lua
+++ nvim/lua/plugins/auto-dark-mode.lua
@@ -4,11 +4,11 @@ return {
 		update_interval = 1000,
 		set_dark_mode = function()
 			vim.api.nvim_set_option("background", "dark")
-			vim.cmd("colorscheme github_dark")
+			-- vim.cmd("colorscheme github_dark")
 		end,
 		set_light_mode = function()
 			vim.api.nvim_set_option("background", "light")
-			vim.cmd("colorscheme github_light")
+			-- vim.cmd("colorscheme github_light")
 		end,
 	},
 }
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,6 @@
 return {
-
-  -- Color schemes
-  { "projekt0n/github-nvim-theme" },
-
-  { "navarasu/onedark.nvim" },
+	{ "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "onedark" } },
+	-- Color schemes
+	{ "projekt0n/github-nvim-theme" },
+	{ "navarasu/onedark.nvim" },
 }
